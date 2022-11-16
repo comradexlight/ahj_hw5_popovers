0f9837a6a7e4dd677b6267d565a34df1181d3700
comradexlight <comradexlight@gmail.com>
2022-11-16 08:41:29 +0300

    from home to work

--- src/js/popover.js
+++ src/js/popover.js
@@ -1,6 +1,20 @@
-export class Popver {
+export class Popover {
   constructor() {
-    this._popovers = [];
+    this._popover = document.createElement('div');
+    this._popover.classList.add('popover');
+    this._popover.innerHTML = ```
+        <div class="arrow"></div>
+        <h3 class="popover-header"></h3>
+        <div class="popover-body"></div>
+        ```
+  }
+
+  showPopover(element) {
+    const popoverHeader = this._popover.querySelector('.popover-header');
+    popoverHeader.textContent = element.title;
+    const popoverBody = this._popover.querySelector('.popover-body');
+    popoverBody.textContent = element.dataset.content;
+    document.body.appendChild(this._popover);
   }
 }
 
