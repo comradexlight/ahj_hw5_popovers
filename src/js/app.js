c95afd8ccf640e7f791f4d57e279c8205b124401
comradexlight <comradexlight@gmail.com>
2022-11-17 08:26:52 +0300

    from home to work

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,33 @@
 import Popover from './popover';
 
-console.log('app.js is bunled');
+const popoverFactory = new Popover();
+const actualPopovers = [];
+const form = document.querySelector('.form');
 
+const showPopover = (el) => {
+  actualPopovers.push({
+    title: el.title,
+    id: popoverFactory.showPopover(el),
+  });
+};
+
+form.addEventListener('click', (e) => {
+  if (e.target.classList.contains('button')) {
+    e.preventDefault();
+    
+
+    console.log(e.target.title)
+    const found = actualPopovers.forEach((el) => {
+      if (el.title === e.target.title) {
+        return true;
+      }
+
+      return false;
+    });
+    if (found) {
+      console.log('here');
+    } else {
+      showPopover(e.target);
+    }
+  }
+});
--- src/js/popover.js
+++ src/js/popover.js
@@ -1,20 +1,34 @@
-export class Popover {
+export default class Popover {
   constructor() {
-    this._popover = document.createElement('div');
-    this._popover.classList.add('popover');
-    this._popover.innerHTML = ```
-        <div class="arrow"></div>
-        <h3 class="popover-header"></h3>
-        <div class="popover-body"></div>
-        ```
+    this._popovers = [];
   }
 
   showPopover(element) {
-    const popoverHeader = this._popover.querySelector('.popover-header');
+    const popoverElement = document.createElement('div');
+    popoverElement.classList.add('popover');
+    const popoverHTML = `
+        <div class="arrow"></div>
+        <h3 class="popover-header"></h3>
+        <div class="popover-body"></div>
+        `;
+    popoverElement.innerHTML = popoverHTML;
+    const popoverHeader = popoverElement.querySelector('.popover-header');
     popoverHeader.textContent = element.title;
-    const popoverBody = this._popover.querySelector('.popover-body');
+    const popoverBody = popoverElement.querySelector('.popover-body');
     popoverBody.textContent = element.dataset.content;
-    document.body.appendChild(this._popover);
+    const id = performance.now();
+    this._popovers.push({
+      id,
+      element: popoverElement,
+    });
+    document.body.appendChild(popoverElement);
+
+    return id;
+  }
+
+  removePopover(id) {
+    const popover = this._popovers.find((p) => p.id === id);
+    popover.element.remove();
+    this._popovers = this._popovers.filter((p) => p.id !== id);
   }
 }
-
